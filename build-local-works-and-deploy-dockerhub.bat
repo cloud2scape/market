0e21e750d5f144858658e0aafcd24e129cfe1597
laigasus <okjaeook98@gmail.com>
2024-11-12 14:25:16 +0900

    build: dockerhub publish script 작성
    개발용 dockerhub 이미지 배포

---
+++ build-local-works-and-deploy-dockerhub.bat
@@ -0,0 +1,18 @@
+cd core-gateway
+docker build -t laigasus/core-gateway:latest .
+docker push laigasus/core-gateway:latest
+
+cd ..
+cd service-account
+docker build -t laigasus/service-account:latest .
+docker push laigasus/service-account:latest
+
+cd ..
+cd service-order
+docker build -t laigasus/service-order:latest .
+docker push laigasus/service-order:latest
+
+cd ..
+cd service-product
+docker build -t laigasus/service-product:latest .
+docker push laigasus/service-product:latest
